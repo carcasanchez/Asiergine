18927d201f7382b18789ccdfd72d48c01e543387
carcasanchez <carcasanchez@gmail.com>
2017-09-16 16:25:49 +0200

    Added module editor to module list and included imgui correctly

--- 3DEngine/Application.cpp
+++ 3DEngine/Application.cpp
@@ -8,6 +8,7 @@ Application::Application()
 	renderer3D = new ModuleRenderer3D(this);
 	camera = new ModuleCamera3D(this);
 	physics = new ModulePhysics3D(this);
+	editor = new ModuleEditor(this);
 
 
 	// The order of calls is very important!
@@ -20,7 +21,7 @@ Application::Application()
 	AddModule(input);
 	AddModule(audio);
 	AddModule(physics);
-	
+	AddModule(editor);
 
 	// Renderer last!
 	AddModule(renderer3D);
--- 3DEngine/Application.h
+++ 3DEngine/Application.h
@@ -10,6 +10,7 @@
 #include "ModuleRenderer3D.h"
 #include "ModuleCamera3D.h"
 #include "ModulePhysics3D.h"
+#include "ModuleEditor.h"
 
 class Application
 {
@@ -20,6 +21,7 @@ public:
 	ModuleRenderer3D* renderer3D;
 	ModuleCamera3D* camera;
 	ModulePhysics3D* physics;
+	ModuleEditor* editor;
 
 
 private:
