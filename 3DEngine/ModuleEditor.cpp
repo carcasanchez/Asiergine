04268dcd9632349efaafedda2e762c558cb38974
asierigle <asier.iglesias@yahoo.es>
2017-10-11 17:48:47 +0200

    Starting style change

--- 3DEngine/ModuleEditor.cpp
+++ 3DEngine/ModuleEditor.cpp
@@ -43,6 +43,51 @@ bool ModuleEditor::Start()
 {
 	bool ret = true;
 	ImGui_ImplSdlGL3_NewFrame(App->window->window);
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.Colors[ImGuiCol_Text] = ImVec4(0.98f, 0.98f, 0.98f, 1.00f);
+	style.Colors[ImGuiCol_TextDisabled] = ImVec4(0.60f, 0.82f, 0.55f, 1.00f);
+	style.Colors[ImGuiCol_WindowBg] = ImVec4(0.00f, 0.08f, 0.38f, 0.45f);
+	style.Colors[ImGuiCol_ChildWindowBg] = ImVec4(0.14f, 0.35f, 0.86f, 0.22f);
+	style.Colors[ImGuiCol_PopupBg] = ImVec4(0.17f, 0.14f, 0.52f, 0.90f);
+	style.Colors[ImGuiCol_Border] = ImVec4(1.00f, 0.00f, 0.00f, 0.40f);
+	style.Colors[ImGuiCol_BorderShadow] = ImVec4(1.00f, 0.00f, 0.00f, 0.45f);
+	style.Colors[ImGuiCol_FrameBg] = ImVec4(1.00f, 0.28f, 0.28f, 0.30f);
+	style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.96f, 0.16f, 0.16f, 0.40f);
+	style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.59f, 0.06f, 0.06f, 0.45f);
+	style.Colors[ImGuiCol_TitleBg] = ImVec4(0.27f, 0.27f, 0.54f, 0.83f);
+	style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.40f, 0.40f, 0.80f, 0.20f);
+	style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.23f, 0.49f, 0.51f, 0.87f);
+	style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.23f, 0.62f, 0.76f, 0.80f);
+	style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.36f, 0.49f, 0.60f, 0.60f);
+	style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.95f, 0.18f, 0.18f, 0.30f);
+	style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.82f, 0.51f, 0.37f, 0.40f);
+	style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.00f, 0.00f, 0.00f, 0.40f);
+	style.Colors[ImGuiCol_ComboBg] = ImVec4(0.71f, 0.42f, 0.42f, 0.99f);
+	style.Colors[ImGuiCol_CheckMark] = ImVec4(0.42f, 0.66f, 0.33f, 0.50f);
+	style.Colors[ImGuiCol_SliderGrab] = ImVec4(0.00f, 0.72f, 0.53f, 0.71f);
+	style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.31f, 0.83f, 0.27f, 1.00f);
+	style.Colors[ImGuiCol_Button] = ImVec4(0.80f, 0.14f, 0.14f, 0.72f);
+	style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.87f, 0.24f, 0.85f, 1.00f);
+	style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.88f, 0.33f, 0.08f, 1.00f);
+	style.Colors[ImGuiCol_Header] = ImVec4(0.00f, 0.00f, 0.98f, 0.45f);
+	style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.21f, 0.21f, 0.60f, 0.80f);
+	style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.09f, 0.09f, 0.60f, 0.80f);
+	style.Colors[ImGuiCol_Separator] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+	style.Colors[ImGuiCol_SeparatorHovered] = ImVec4(0.60f, 0.60f, 0.70f, 1.00f);
+	style.Colors[ImGuiCol_SeparatorActive] = ImVec4(0.70f, 0.70f, 0.90f, 1.00f);
+	style.Colors[ImGuiCol_ResizeGrip] = ImVec4(1.00f, 1.00f, 1.00f, 0.30f);
+	style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(1.00f, 1.00f, 1.00f, 0.60f);
+	style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(1.00f, 1.00f, 1.00f, 0.90f);
+	style.Colors[ImGuiCol_CloseButton] = ImVec4(0.50f, 0.50f, 0.90f, 0.50f);
+	style.Colors[ImGuiCol_CloseButtonHovered] = ImVec4(0.70f, 0.70f, 0.90f, 0.60f);
+	style.Colors[ImGuiCol_CloseButtonActive] = ImVec4(0.70f, 0.70f, 0.70f, 1.00f);
+	style.Colors[ImGuiCol_PlotLines] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+	style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+	style.Colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+	style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+	style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.00f, 0.00f, 1.00f, 0.35f);
+	style.Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(0.20f, 0.20f, 0.20f, 0.35f);
+
 
 	//reserve for fps_log
 	fps_log = std::vector<float>(MAX_FPS_LOG, 0);
