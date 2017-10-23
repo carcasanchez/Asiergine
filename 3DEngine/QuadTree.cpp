7ce7a7a325aaab4b37b98272c965f1ae0e7ba483
asierigle <asier.iglesias@yahoo.es>
2017-10-23 02:38:20 +0200

    Trying some things with the QuadTree

--- 3DEngine/QuadTree.cpp
+++ 3DEngine/QuadTree.cpp
@@ -1,10 +1,7 @@
 #include "QuadTree.h"
+#include <vector>
 
-template<typename TYPE>
-QuadTreeNodeObj<TYPE>::QuadTreeNodeObj(float3 center, float3 half_width): center(center), half_width(half_width)
-{
-}
-
+//Node functions--------------------------------
 template<typename TYPE>
 QuadTreeNodeObj<TYPE>::~QuadTreeNodeObj()
 {
@@ -17,18 +14,32 @@ math::AABB QuadTreeNodeObj<TYPE>::Create(float3 center, float3 half_width)
 }
 
 template<typename TYPE>
-void QuadTreeNodeObj<TYPE>::Insert(GameObject * game_object)
+void QuadTreeNodeObj<TYPE>::Insert(GameObject* game_object)
 {
+	game_objects.push_back(game_object);
 }
 
 template<typename TYPE>
-void QuadTreeNodeObj<TYPE>::Remove(GameObject * game_object)
+void QuadTreeNodeObj<TYPE>::Remove(GameObject* game_object)
 {
+	game_objects.pop_back(game_object);
 }
 
 template<typename TYPE>
 void QuadTreeNodeObj<TYPE>::Clear()
 {
+	std::vector<QuadTreeNodeObj*>::iterator it = children.end();
+	while (it != children.begin())
+	{
+		children.pop_back((*it));
+		--it;
+	}
+	std::vector<GameObject*>::iterator it1 = game_objects.end();
+	while (it1 != game_objects.begin())
+	{
+		game_objects.pop_back((*it));
+		--it;
+	}
 }
 
 template<typename TYPE>
@@ -37,10 +48,31 @@ bool QuadTreeNodeObj<TYPE>::Intersect(std::vector<GameObject*>& game_bjects, con
 	return false;
 }
 
-template<typename TYPE>
-QuadTreeObj<TYPE>::QuadTreeObj()
+
+//QuadTree functions---------------------------------------
+QuadTreeObj::~QuadTreeObj()
 {}
 
-template<typename TYPE>
-QuadTreeObj<TYPE>::~QuadTreeObj()
-{}
+void QuadTreeObj::Draw()
+{
+	int i = 0;
+	root->Create(float3::zero, { 12,12,12 });
+	std::vector<GameObject*>::iterator it = game_objects.begin();
+	while (it != game_objects.end())
+	{
+		++i;
+		++it;
+	}
+	if (i > root->GetMaxGameObjects())
+	{
+		root->Create({ 1.5,6,1.5 }, { 3,12,3 });
+		root->Create({ -1.5,6,-1.5 }, { 3,12,3 });
+		root->Create({ 1.5,6,-1.5 }, { 3,12,3 });
+		root->Create({ -1.5,6,1.5 }, { 3,12,3 });
+	}
+
+	
+}
+
+
+
--- 3DEngine/QuadTree.h
+++ 3DEngine/QuadTree.h
@@ -6,31 +6,34 @@ template <typename TYPE>
 class QuadTreeNodeObj
 {
 public:
-	QuadTreeNodeObj(float3 center, float3 half_width);
+	QuadTreeNodeObj(float3 size, std::vector<QuadTreeNodeObj*>* children, int max_game_objects) : size(size),children(children),max_game_objects(max_game_objects) {}
 	~QuadTreeNodeObj();
 	math::AABB Create(float3 center, float3 half_width);
 
 	void Insert(GameObject* game_object);
 	void Remove(GameObject* game_object);
-
+	int GetMaxGameObjects() { return max_game_objects; }
 	void Clear();
 
 	bool Intersect(std::vector<GameObject*> &game_bjects, const TYPE &primitive);
-
-
-private:
-	math::float3 center;
-	math::float3 half_width;
+	
+private:	
+	math::float3 size = float3::zero;
+	std::vector<QuadTreeNodeObj*> children = nullptr;
+	int max_game_objects = 6;
+	std::vector<GameObject*> game_objects;
 };
 
-template<typename TYPE>
+
 class QuadTreeObj
 {
 public:
-	QuadTreeObj();
+	QuadTreeObj() {}
 	~QuadTreeObj();
-
+	void Draw();
 private:
-	QuadTreeNodeObj root = nullptr;
+	QuadTreeNodeObj<math::AABB>* root;
+	std::vector<GameObject*> game_objects;
+	
 };
 
